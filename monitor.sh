ebcd62c6ae449221c4f927aa3bffd33349181927
Mikael Vester <pmvester@gmail.com>
2017-04-26 13:25:10 +0200

    added script for monitoring

---
+++ monitor.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+org="qc65wr"
+dtype="MyDeviceType"
+did="myDevice"
+apikey="a-qc65wr-bplwfjbxvp"
+token="DHxH-pH*unNwN2jCaQ"
+
+mosquitto_sub -h $org.messaging.internetofthings.ibmcloud.com -t "iot-2/type/+/id/+/mon" -i "a:$org:$apikey" -P "$token" -u "$apikey" -q 1 
