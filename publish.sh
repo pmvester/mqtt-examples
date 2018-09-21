88320158705ef1c860e5132246b63d8ed4a446b3
Mikael Vester <pmvester@gmail.com>
2018-09-21 11:11:58 +0200

    new call paramters

--- publish.sh
+++ publish.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-org="mqnl6k"
-dtype="MyDeviceType"
-did="myDevice"
-token="20171009"
+orgId="b6824e"
+appId="a-b6824e-ltp0wdw6ib"
+token="sakTi)UDn37Kps(iL+"
+dtype="DnxDevice"
+did="dnxDevice1"
 topic="k34/heat/feed/water"
 
-# base64 encoding of topic hierarchy to bypass the flat limitation
-# imposed by IBM IoT Platform. Here as an example, not a requirement
-# if a flat structure is ok.
 b64topic=$(echo $topic | base64)
 n=0
 
@@ -20,7 +18,7 @@ do
 
   json="{\"time\": $ts, \"value\": $n}"
 
-  mosquitto_pub -h $org.messaging.internetofthings.ibmcloud.com -t "iot-2/evt/$b64topic/fmt/json" -m "$json" -i "d:$org:$dtype:$did" -P "$token" -u "use-token-auth" -q 1
+  mosquitto_pub -h $orgId.messaging.internetofthings.ibmcloud.com -t "iot-2/type/DnxDevice/id/dnxDevice1/evt/$b64topic/fmt/json" -m "$json" -i "a:$orgId:$appId" -P "$token" -u "a-b6824e-ltp0wdw6ib" -q 1 
 
   echo "publishing $json"
   sleep 1
