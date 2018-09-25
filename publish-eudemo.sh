f57c0973cbb46635783ecc687dc3649d4c6a6fb2
Mikael Vester <mikael.vester@se.ibm.com>
2018-09-25 15:52:29 +0200

    new file + minor changes

---
+++ publish-eudemo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+did="MV_pseudoDevice1"
+dtype="MV_PseudoDevice"
+orgId="s8sb6o"
+token="1221080932"
+topic="temp"
+
+n=0
+
+while true
+do
+
+  n=$[n+1]
+  ts=$(date +%s)
+
+  json="{\"time\": $ts, \"value\": $n}"
+
+  mosquitto_pub -h $orgId.messaging.internetofthings.ibmcloud.com -t "iot-2/evt/$topic/fmt/json" -m "$json" -i "d:$orgId:$dtype:$did" -P "$token" -u "use-token-auth" -q 1 
+
+  echo "publishing $json"
+  sleep 1
+
+done
