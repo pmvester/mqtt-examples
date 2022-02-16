fcad501d20cd7a54fc345067480a49e8300790d1
Mikael Vester <mikael.vester@se.ibm.com>
2022-02-16 07:26:58 +0100

    added publishing example using tls on port 443

---
+++ publish-tls.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+did="k34TempOutdoor"
+dtype="K34Temperature"
+orgId="masdev"
+token="1221080932"
+topic="temp"
+
+n=0
+
+while true
+do
+
+  # create a random numeric value in the range 0 to 99
+  n=$(($RANDOM % 100))
+  ts=$(date +%s)
+
+  json="{\"time\": $ts, \"value\": $n}"
+
+  mosquitto_pub -L mqtts://$orgId.messaging.iot.iot1.imomax.org/"iot-2/evt/$topic/fmt/json" \
+                -m "$json" \
+                -i "d:$orgId:$dtype:$did" \
+                -P "$token" \
+                -u "use-token-auth" \
+                -q 1 \
+                --cafile ca.crt \
+                --cert client.crt \
+                --key client.key \
+                -p 443
+
+  echo "publishing $json"
+  sleep 1
+
+done
