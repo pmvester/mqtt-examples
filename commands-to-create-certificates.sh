4b2b04928fe66542796827417d640cfdeb157831
Mikael Vester <mikael.vester@se.ibm.com>
2022-02-16 09:54:05 +0100

    cert example

---
+++ commands-to-create-certificates.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# http://www.steves-internet-guide.com/creating-and-using-client-certificates-with-mqtt-and-mosquitto/
+
+openssl genrsa -des3 -out ca.key 2048
+
+openssl genrsa -out client.key 2048
+
+openssl req -new -out client.csr -key client.key
+
+openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client.crt -days 360
+
+# mosquitto_pub --cafile ca.crt --cert client.crt --key client.key -d -h 192.168.1.157 -p 8883 -t test -m "hello there"
+
